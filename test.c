5f074c1ba6ade722a2686e6ff7b454fd71aa6549
Yuri Geovani <yuri_geovani@hotmail.com>
2023-06-04 01:34:17 +0100

    finally

--- test.c
+++ test.c
@@ -1,35 +1,36 @@
 #include<stdio.h>
 
 int main() {
-    int opcao = 2;
+    char opcao = 49;
 
-    while (opcao!=0)
+    while (opcao!=48)
     {
         printf("\n1) Opcao 1");
         printf("\n2) Opcao 2");
         printf("\n3) Opcao 3");
         printf("\n0) SAIR");
         printf("\nInforme a opcao: ");
-        scanf("%d", &opcao);
+        scanf(" %c", &opcao);
 
+        // printf("Opcao: %c", opcao);
         switch (opcao)
         {
-        case 1:
-            printf("\nOpcao 1 selecionada!");
+        case 49:
+            printf("Opcao 1 selecionada!");
             break;
-        case 2:
-            printf("\nOpcao 2 selecionada!");
+        case 50:
+            printf("Opcao 2 selecionada!");
             break;
-        case 3:
-            printf("\nOpcao 3 selecionada!");
+        case 51:
+            printf("Opcao 3 selecionada!");
             break;
-        case 0:
+        case 48:
             break;
         default:
-            printf("\nOpcao invalida! Selecione uma opcao valida!");
+            printf("Opcao invalida! Selecione uma opcao correta!");
             break;
         }
     }
-    
+
     return 0;
 }
